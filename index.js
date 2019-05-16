156568a6cbf6121351f893648496b5a2680b7656
Finn Jordal <finn@jordal.dk>
2019-05-16 14:35:40 +0200

    Geolocation

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import {get as getProjection} from 'ol/proj';
 import TileWMS from 'ol/source/TileWMS.js';
 import WMTS from 'ol/source/WMTS.js';
 import WMTSTileGrid from 'ol/tilegrid/WMTS.js';
+import Geolocation from 'ol/Geolocation.js';
 
 proj4.defs('EPSG:25832', "+proj=utm +zone=32 +ellps=GRS80 +units=m +no_defs");
 register(proj4);
@@ -19,6 +20,15 @@ var kfTileGrid = new WMTSTileGrid({
   resolutions: [1638.4,819.2,409.6,204.8,102.4,51.2,25.6,12.8,6.4,3.2,1.6,0.8,0.4,0.2],
   matrixIds: ['L00','L01','L02','L03','L04','L05','L06','L07','L08','L09','L10','L11','L12','L13'],
 });
+
+const view= new View({
+    minZoom: 2,
+    maxZoom: 13,
+    center: [654500, 6176450], // start center position
+    zoom: 9, // start zoom level
+    resolutions: [1638.4,819.2,409.6,204.8,102.4,51.2,25.6,12.8,6.4,3.2,1.6,0.8,0.4,0.2,0.1], // Equal to WMTS resolutions with three more detailed levels
+    projection: dkProjection // use our custom projection defined earlier
+  })
 
 let kftoken= 'd23aed4ea6f89420aae2fcf89b47e95b';
 
@@ -42,29 +52,37 @@ const map = new Map({
     //     }
     //   })
     // })
-
+    
     new TileLayer({
-      opacity: 1.0,
-      title:'Skærmkort',
-      type:'base',
-      visible: true, // by default this layer is visible
-      source: new WMTS({ 
-        url: "https://services.kortforsyningen.dk/topo_skaermkort?token="+kftoken,
-        layer: "dtk_skaermkort",
-        matrixSet: "View1",
-        format: "image/jpeg",
-        tileGrid: kfTileGrid,
-        style: 'default',
-        size: [256, 256]
-      })
-    })
+      source: new OSM()
+    }),
+    // new TileLayer({
+    //   opacity: 1.0,
+    //   title:'Skærmkort',
+    //   type:'base',
+    //   visible: true, // by default this layer is visible
+    //   source: new WMTS({ 
+    //     url: "https://services.kortforsyningen.dk/topo_skaermkort?token="+kftoken,
+    //     layer: "dtk_skaermkort",
+    //     matrixSet: "View1",
+    //     format: "image/jpeg",
+    //     tileGrid: kfTileGrid,
+    //     style: 'default',
+    //     size: [256, 256]
+    //   })
+    // })
   ],
-  view: new View({
-    minZoom: 2,
-    maxZoom: 13,
-    center: [654500, 6176450], // start center position
-    zoom: 9, // start zoom level
-    resolutions: [1638.4,819.2,409.6,204.8,102.4,51.2,25.6,12.8,6.4,3.2,1.6,0.8,0.4,0.2,0.1], // Equal to WMTS resolutions with three more detailed levels
-    projection: dkProjection // use our custom projection defined earlier
-  })
+  view: view
+});
+
+var geolocation = new Geolocation({
+    // enableHighAccuracy must be set to true to have the heading value.
+    trackingOptions: {
+      enableHighAccuracy: true
+    },
+    projection: view.getProjection()
+  });
+geolocation.setTracking(true);
+geolocation.on('change', function(evt) {
+  view.setCenter(geolocation.getPosition());
 });
