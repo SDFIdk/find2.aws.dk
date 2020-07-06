c13c0fc726a6676a1513ea9dae496ce70bfbb3f9
Finn Jordal <finn@jordal.dk>
2020-07-06 16:17:53 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 
 // CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'find2awsdk3';
+const CACHE_NAME = 'find2awsdk4';
 
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
